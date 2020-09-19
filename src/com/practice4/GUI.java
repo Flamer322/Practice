7a721e4b61fca813785b4b534b66a44102790e93
Flamer322 <b.bar1enev@gmail.com>
2020-09-19 13:28:29 +0300

    upd

--- src/com/practice4/GUI.java
+++ src/com/practice4/GUI.java
@@ -2,18 +2,64 @@ package com.practice4;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
 
-    JTextField jtf = new JTextField(10);
-    Font fnt = new Font("Times new roman", Font.BOLD,20);
+    int sc1;
+    int sc2;
+
+    JButton button1 = new JButton("AC Milan");
+    JButton button2 = new JButton("Real Madrid");
+    JLabel label1 = new JLabel("Result: " + sc1 + " X " + sc2);
+    JLabel label2 = new JLabel("Last Scorer: N/A");
+    JLabel label3 = new JLabel("Winner: DRAW");
+
     GUI() {
-        super("GUI");
-        setLayout(new FlowLayout());
-        setSize(300, 100);
-        add(jtf);
-        jtf.setForeground(Color.gray);
-        jtf.setFont(fnt);
+        super("Score");
+        setLayout(null);
+        setSize(600, 300);
+        button1.setBounds(50, 50, 150, 30);
+        button2.setBounds(250, 50, 150, 30);
+        label1.setBounds(50, 100, 250, 30);
+        label2.setBounds(50, 150, 250, 30);
+        label3.setBounds(50, 200, 250, 30);
+        add(button1);
+        add(button2);
+        add(label1);
+        add(label2);
+        add(label3);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sc1++;
+                label1.setText("Result: " + sc1 + " X " + sc2);
+                label2.setText("Last Scorer: AC Milan");
+                if (sc1 > sc2){
+                    label3.setText("Winner: AC Milan");
+                } else if (sc1 == sc2) {
+                    label3.setText("Winner: DRAW");
+                } else {
+                    label3.setText("Winner: Real Madrid");
+                }
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sc2++;
+                label1.setText("Result: " + sc1 + " X " + sc2);
+                label2.setText("Last Scorer: Real Madrid");
+                if (sc1 > sc2){
+                    label3.setText("Winner: AC Milan");
+                } else if (sc1 == sc2) {
+                    label3.setText("Winner: DRAW");
+                } else {
+                    label3.setText("Winner: Real Madrid");
+                }
+            }
+        });
         setVisible(true);
     }
 
